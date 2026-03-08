fa611e6eee69bd4657d2ca3252a855f9e13eecbf
shu-vro <official.shirshen@gmail.com>
2026-03-08 19:53:53 +0600

    chore: update logging for dl server

--- download-server/config/logging.py
+++ download-server/config/logging.py
@@ -6,12 +6,12 @@ def configure_logging(log_level: str) -> None:
     logger.remove()
     logger.add(
         sys.stderr,
-        format="<level>{level: <8}</level> [{time:YYYY-MM-DD HH:mm:ss.SSS ZZZ}] <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        format="<level>{level: <8}</level> [{time:YYYY-MM-DD HH:mm:ss.SSS Z}] <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
         level=log_level,
     )
     logger.add(
         "logs/app.log",
-        format="{level: <8} [{time:YYYY-MM-DD HH:mm:ss.SSS ZZZ}] {name}:{function}:{line} - {message}",
+        format="{level: <8} [{time:YYYY-MM-DD HH:mm:ss.SSS Z}] {name}:{function}:{line} - {message}",
         level="DEBUG",
         rotation="500 MB",
         retention="1 month",
